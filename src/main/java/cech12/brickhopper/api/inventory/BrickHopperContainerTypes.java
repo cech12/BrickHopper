85cf5902495fd111a92548a2e439c64514934ed3
Christian Voß <cech12@gmail.com>
2021-12-04 09:11:24 +0100

    Update mod to Forge 1.18-38.0.4

--- src/main/java/cech12/brickhopper/api/inventory/BrickHopperContainerTypes.java
+++ src/main/java/cech12/brickhopper/api/inventory/BrickHopperContainerTypes.java
@@ -6,13 +6,13 @@ import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
-import net.minecraftforge.common.extensions.IForgeContainerType;
+import net.minecraftforge.common.extensions.IForgeMenuType;
 
 public class BrickHopperContainerTypes {
 
     public final static ResourceLocation BRICK_HOPPER_ID = new ResourceLocation(BrickHopperMod.MOD_ID, "brickhopper");
 
-    public static MenuType<? extends AbstractContainerMenu> BRICK_HOPPER = IForgeContainerType.create((pWindowID, pInventory, pData) -> {
+    public static MenuType<? extends AbstractContainerMenu> BRICK_HOPPER = IForgeMenuType.create((pWindowID, pInventory, pData) -> {
         BlockPos pos = pData.readBlockPos();
         return new BrickHopperContainer(pWindowID, pInventory, pos);
     }).setRegistryName(BRICK_HOPPER_ID);
