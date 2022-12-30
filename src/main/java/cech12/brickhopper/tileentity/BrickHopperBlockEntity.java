d43eb729dfe53d9e2dd75b1de3ad4fdac819fe75
Christian Voß <cech12@gmail.com>
2022-12-30 11:51:19 +0100

    fixed: hopper did not fill up with the item it contained even though there was space

--- src/main/java/cech12/brickhopper/tileentity/BrickHopperBlockEntity.java
+++ src/main/java/cech12/brickhopper/tileentity/BrickHopperBlockEntity.java
@@ -179,7 +179,7 @@ public class BrickHopperBlockEntity extends RandomizableContainerBlockEntity imp
 
     private static ItemStack insertStack(BlockEntity source, Object destination, IItemHandler destInventory, ItemStack stack, int slot) {
         ItemStack itemstack = destInventory.getStackInSlot(slot);
-        if (destInventory.insertItem(slot, stack, true).isEmpty()) {
+        if (destInventory.insertItem(slot, stack, true) != stack) {
             boolean insertedItem = false;
             boolean inventoryWasEmpty = isEmpty(destInventory);
             if (itemstack.isEmpty()) {
