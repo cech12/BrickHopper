530b229021947a6ad41d9111948d561e27eb8778
cech12 <cech12@gmail.com>
2026-09-22 20:20:09 +0200

    fixed: shift click an item stack into a brick hopper with said item already in it deleted the item stack (NeoForge) https://github.com/cech12/WoodenHopper/issues/50

--- common/src/main/java/de/cech12/brickhopper/inventory/BrickHopperContainer.java
+++ common/src/main/java/de/cech12/brickhopper/inventory/BrickHopperContainer.java
@@ -23,15 +23,7 @@ public class BrickHopperContainer extends AbstractContainerMenu {
             this.addSlot(new Slot(inventory, j, 62 + j * 18, 20));
         }
         //inventory
-        for(int l = 0; l < 3; ++l) {
-            for(int k = 0; k < 9; ++k) {
-                this.addSlot(new Slot(playerInventory, k + l * 9 + 9, 8 + k * 18, l * 18 + 51));
-            }
-        }
-
-        for(int i1 = 0; i1 < 9; ++i1) {
-            this.addSlot(new Slot(playerInventory, i1, 8 + i1 * 18, 109));
-        }
+        this.addStandardInventorySlots(playerInventory, 8, 51);
     }
 
     public BrickHopperContainer(int id, Inventory playerInventoryIn) {
@@ -67,7 +59,7 @@ public class BrickHopperContainer extends AbstractContainerMenu {
             }
 
             if (itemstack1.isEmpty()) {
-                slot.set(ItemStack.EMPTY);
+                slot.setByPlayer(ItemStack.EMPTY);
             } else {
                 slot.setChanged();
             }
